a0c7e65a88c31508f6678dd988f31718bb60c006
Zack Guard <zacharyguard@gmail.com>
2021-02-13 22:03:43 +0800

    Add some docs

--- include/zopt.h
+++ include/zopt.h
@@ -22,8 +22,37 @@ typedef struct ZOPT_OPTS {
     int args_count;
 } ZOPT_OPTS;
 
+/*
+ * Parse command line options.
+ * 
+ * argc - The number of items in argv.
+ * argv - The argument vector.
+ * opt_defs - The option definitions. See ZOPT_DEF above.
+ * opt_defs_count - The number of option definitions.
+ * 
+ * Returns an options container; see ZOPT_OPTS above.
+ */
 ZOPT_OPTS zopt_parse(int argc, const char *argv[], ZOPT_DEF opt_defs[], int opt_defs_count);
 
+/*
+ * Get the value and kind of a specified option.
+ * 
+ * opts - The options container.
+ * name - The name of the option whose value to get.
+ * value_out - The address of the buffer to which to write the option's value.
+ * value_out_size - The size of the buffer.
+ * 
+ * Returns the kind of the specified option, or ZOPT_NOTPRESENT if it is not present.
+ */
 ZOPT_KIND zopt_get(ZOPT_OPTS opts, const char *name, char *value_out, int value_out_size);
 
+/*
+ * Get the value of a specified boolean option.
+ * 
+ * opts - The options container.
+ * name - The name of the option whose value to get.
+ * default_val - The value to return if the specified option is not present.
+ * 
+ * Returns the value of the specified option, or default_val if it is not present.
+ */
 char zopt_get_bool(ZOPT_OPTS opts, const char *name, char default_val);
