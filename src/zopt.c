d38f802b077a6badb6f31bc18c8f8f4a7e4a7a0e
Zack Guard <zacharyguard@gmail.com>
2021-02-13 18:31:54 +0800

    Support compound and long options
    e.g. -zxvf and --foo respectively. The latter does not (yet) support the --foo=bar syntax and is parsed as a single "foo=bar" option.

--- src/zopt.c
+++ src/zopt.c
@@ -3,13 +3,13 @@
 #include <string.h>
 #include "zopt.h"
 
-#define TRUE    1
-#define FALSE   0
+typedef char BOOL;
+
+#define TRUE    (BOOL)1
+#define FALSE   (BOOL)0
 
 #define DUMP_STR(name)  printf("%s = \"%s\"\n", #name, name)
 #define DUMP_INT(name)  printf("%s = %d\n", #name, name)
-
-typedef char BOOL;
 
 ZOPT_VAL *add_opt(ZOPT_VAL opts[], int opts_count, ZOPT_DEF *def, const char *value) {
     opts = (ZOPT_VAL *)realloc(opts, (opts_count+1) * sizeof(ZOPT_VAL));
@@ -28,6 +28,23 @@ ZOPT_DEF *find_def(ZOPT_DEF opt_defs[], int opt_defs_count, const char *name) {
     return 0;
 }
 
+void _zopt_parse_handle_opt(ZOPT_OPTS *result, ZOPT_DEF opt_defs[], int opt_defs_count, const char *cur_opt_name, BOOL *is_waiting_for_val, ZOPT_DEF **cur_def) {
+    *cur_def = find_def(opt_defs, opt_defs_count, cur_opt_name);
+    if (!*cur_def) {
+        fprintf(stderr, "Unknown option '%s'\n", cur_opt_name);
+        return;
+    }
+    switch ((*cur_def)->kind) {
+        case ZOPT_BOOL:
+            *is_waiting_for_val = FALSE;
+            result->opts = add_opt(result->opts, result->count++, *cur_def, "");
+            break;
+        case ZOPT_STR:
+            *is_waiting_for_val = TRUE;
+            break;
+    }
+}
+
 ZOPT_OPTS zopt_parse(int argc, const char *argv[], ZOPT_DEF opt_defs[], int opt_defs_count) {
     ZOPT_OPTS result;
     result.opts = NULL;
@@ -35,7 +52,7 @@ ZOPT_OPTS zopt_parse(int argc, const char *argv[], ZOPT_DEF opt_defs[], int opt_
     result.args = NULL;
     result.args_count = 0;
 
-    BOOL is_waiting_for_val = 0;
+    BOOL is_waiting_for_val = FALSE;
     ZOPT_DEF *cur_def;
     for (int i = 1; i < argc; ++i) {
         if (is_waiting_for_val) {
@@ -44,24 +61,27 @@ ZOPT_OPTS zopt_parse(int argc, const char *argv[], ZOPT_DEF opt_defs[], int opt_
             continue;
         }
         
-        if (argv[i][0] != '-') {
+        int len;
+        if (argv[i][0] != '-' || (len = strlen(argv[i])) < 2) {
             ++result.args_count;
             result.args = (const char **)realloc(result.args, result.args_count * sizeof(const char *));
             result.args[result.args_count - 1] = argv[i];
             continue;
         }
 
-        const char *cur_opt_name = &argv[i][1];
-        cur_def = find_def(opt_defs, opt_defs_count, cur_opt_name);
-        if (!cur_def) {
-            fprintf(stderr, "Unknown option '%s'\n", cur_opt_name);
-            continue;
-        }
-        switch (cur_def->kind) {
-            case ZOPT_BOOL:
-                result.opts = add_opt(result.opts, result.count++, cur_def, ""); break;
-            case ZOPT_STR:
-                is_waiting_for_val = TRUE;
+        const char *cur_opt_name;
+        BOOL is_long_opt = argv[i][1] == '-'; // whole thing starts with '--'
+        if (is_long_opt) {
+            cur_opt_name = &argv[i][2];
+            _zopt_parse_handle_opt(&result, opt_defs, opt_defs_count, cur_opt_name, &is_waiting_for_val, &cur_def);
+        } else {
+            for (int j = 1; j < len; ++j) {
+                char opt_name[2];
+                opt_name[0] = argv[i][j];
+                opt_name[1] = 0;
+                cur_opt_name = opt_name;
+                _zopt_parse_handle_opt(&result, opt_defs, opt_defs_count, cur_opt_name, &is_waiting_for_val, &cur_def);
+            }
         }
     }
     
@@ -76,6 +96,7 @@ ZOPT_KIND zopt_get(ZOPT_OPTS opts, const char *name, char *value_out, int value_
             return opt->kind;
         }
     }
+    strcpy(value_out, "");
     return ZOPT_NOTPRESENT;
 }
 
@@ -94,15 +115,20 @@ int main(int argc, const char *argv[]) {
     ZOPT_DEF opt_defs[] = {
         { .name = "m", .kind = ZOPT_BOOL },
         { .name = "p", .kind = ZOPT_BOOL },
-        { .name = "u", .kind = ZOPT_STR }
+        { .name = "u", .kind = ZOPT_STR },
+        { .name = "foo", .kind = ZOPT_STR }
     };
     ZOPT_OPTS opts = zopt_parse(argc, argv, opt_defs, sizeof(opt_defs) / sizeof(ZOPT_DEF));
     printf("parsed %d opts and %d args\n", opts.count, opts.args_count);
 
     printf("-m = %d\n", zopt_get_bool(opts, "m", FALSE));
     printf("-p = %d\n", zopt_get_bool(opts, "p", FALSE));
-    char u_val[128]; zopt_get(opts, "u", u_val, sizeof(u_val));
-    printf("-u = \"%s\"\n", u_val);
+    char u_val[128];
+    if (zopt_get(opts, "u", u_val, sizeof(u_val)))
+        printf("-u = \"%s\"\n", u_val);
+    char foo_val[128];
+    if (zopt_get(opts, "foo", foo_val, sizeof(foo_val)))
+        printf("--foo = \"%s\"\n", foo_val);
 
     for (int i = 0; i < opts.args_count; ++i) {
         printf("arg: \"%s\"\n", opts.args[i]);
